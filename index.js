64d0817646370ba4169ff3261a2fafb43a5f480b
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-03-18 13:07:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, nameCheck) {
+    const result = drivers.filter(name => name.toUpperCase() === nameCheck.toUpperCase())
+        return result
+}
+
+function fuzzyMatch(drivers, stringCheck) {
+    const result = drivers.filter(firstLetters => firstLetters.toUpperCase().startsWith(stringCheck.toUpperCase()))
+    return result
+}
+
+function matchName(drivers, checkName) {
+    const result = drivers.filter(drivers => drivers.name.toUpperCase() === checkName.toUpperCase())
+    return result
+}
